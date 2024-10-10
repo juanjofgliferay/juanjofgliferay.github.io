34829470a6a1c6bf5c0fad1a146fbf610cf0d764
Juanjo Fernandez <juanjo.fernandez@liferay.com>
2024-10-10 15:18:04 +0200

    Structure project and configure vite to transpile ts

---
+++ lr/cx/filters/sampleFdsFilter/src/index.ts
@@ -0,0 +1,53 @@
+type FilterData = string;
+
+function descriptionBuilder(selectedData: FilterData): string {
+	return selectedData;
+}
+
+function htmlElementBuilder({
+	fieldName,
+	filter,
+	setFilter,
+}: any): HTMLElement {
+	const input = document.createElement('input');
+
+	if (filter.selectedData) {
+		input.value = filter.selectedData;
+	}
+	else {
+		input.value = `${fieldName} eq ...`;
+	}
+
+	input.className = 'form-control';
+	input.placeholder = 'Search with Odata';
+
+	const button = document.createElement('button');
+
+	button.className = 'btn btn-block btn-secondary btn-sm mt-2';
+	button.innerText = 'Submit';
+	button.onclick = () =>
+		setFilter({
+			selectedData: input.value,
+		});
+
+	const div = document.createElement('div');
+
+	div.className = 'dropdown-item';
+
+	div.appendChild(input);
+	div.appendChild(button);
+
+	return div;
+}
+
+function oDataQueryBuilder(selectedData: FilterData): string {
+	return selectedData;
+}
+
+const fdsFilter = {
+	descriptionBuilder,
+	htmlElementBuilder,
+	oDataQueryBuilder,
+};
+
+export default fdsFilter;
